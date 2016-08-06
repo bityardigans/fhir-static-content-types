be3aa619380e63ab46c9fb6e45e8bd4f9d0345ba
Josh Mandel <jmandel@med.harvard.edu>
2016-08-06 14:18:20 -0400

    Add apache example

---
+++ example-apache.sh
@@ -0,0 +1,9 @@
+wget http://hl7.org/fhir/fhir-spec.zip
+unzip fhir-spec.zip
+cp apache/htaccess site/.htaccess
+
+docker run \
+    -p 8000:80 \
+    -v $(pwd)/apache/httpd.conf:/usr/local/apache2/conf/httpd.conf:ro \
+    -v $(pwd)/site:/usr/local/apache2/htdocs:ro \
+    httpd
