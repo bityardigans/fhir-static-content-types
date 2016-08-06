1a6cb3304670e9d5283c8146a1d36d23802b3e75
Josh Mandel <jmandel@alum.mit.edu>
2016-08-05 23:15:46 -0400

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,8 @@
 wget http://hl7.org/fhir/fhir-spec.zip
 unzip fhir-spec.zip
 
-docker run --name fhir-nginx \
+docker run
+    -p 8000:80 \
     -v $(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro \
     -v $(pwd)/site:/usr/share/nginx/html:ro \
     -d nginx
